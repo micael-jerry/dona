14da8f526f10f9c437b159e7686f0b7048d3676f
As Manjaka Josvah Rakotondrabe <as-rakot@c1r9s5.42antananarivo.mg>
2026-09-12 16:42:55 +0300

    fix(openapi-ts): update openapi config to use local backend api

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-	input: 'https://dona-api-theta.vercel.app/swagger/json',
+	// input: 'https://dona-api-theta.vercel.app/swagger/json',
+	input: 'http://localhost:8080/swagger/json',
 	output: 'client',
 	plugins: ['@hey-api/client-axios'],
 });
